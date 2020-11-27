5edf8e80ebd0e8c14f46c29766770bc88927a9f1
Mihail M <r@gothundead.ru>
2020-11-28 01:00:43 +0300

    npm bootstrap, jquery, popper

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ isset($title) ? $title : 'Тестовое задание' }} </title>
+    @yield('head_styles')
+    @yield('head_scripts')
+</head>
+<body>
+@yield('body')
+@yield('bottom_scripts')
+</body>
+</html>
