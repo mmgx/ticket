8ddc91a956fcfa8f0f379f5be2850088ffcc7bd6
Mihail M <r@gothundead.ru>
2020-11-29 12:53:06 +0300

    Добавлены контроллеры

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Base\BaseController;
+use App\Service\EventsService;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class EventController extends BaseController
+{
+    protected $eventsService;
+
+    public function __construct(EventsService $eventsService)
+    {
+        $this->eventsService = $eventsService;
+    }
+
+    /**
+     * вывод списка мероприятий
+     * @return Application|Factory|View
+     * @throws GuzzleException
+     */
+    public function index()
+    {
+        $shows = $this->eventsService->getShows();
+        return view('pages.events.index')->withShows($shows);
+    }
+
+    /**
+     * детальная страница мероприятия со списком событий
+     * @param $id
+     * @return Application|Factory|View
+     * @throws GuzzleException
+     */
+    public function show($showId)
+    {
+        $events = $this->eventsService->getEvents($showId);
+        return view('pages.events.show')->withEvents($events);
+    }
+
+    /**
+     * Получить список мест
+     * @param $eventId
+     * @return Application|Factory|View
+     * @throws GuzzleException
+     */
+    public function places($eventId)
+    {
+        $places = $this->eventsService->getPlaces($eventId);
+
+        foreach($places as $place){
+            $schemeData[] = [
+                'Angle' => "0.00",
+                'BackColor' => "66CCCC",
+                'CX' => $place['x'],
+                'CX2' => $place['x'],
+                'CY' => $place['y'],
+                'CY2' => $place['y'],
+                'FontColor' => "",
+                'FontSize' => "0",
+                'FreeOfferSeat' => "0",
+                'Height' => "30",
+                'ID' => $place['id'],
+                'Label' => "0",
+                'MaxX' => "13043",
+                'MaxY' => "7563",
+                'MinX' => "3529",
+                'MinY' => "5105",
+                'Name_sec' => "Название местар",
+                'NomBilKn' => "1460",
+                'ObjectName' => "Place",
+                'ObjectType' => "Place",
+                'Price' => "1000",
+                'PriceSell' => "1000.0000",
+                'Row' => $place['y'],
+                'Seat' => $place['x'],
+                'Width' => "30",
+                'avail' => $place['is_available'],
+                'cod_sec' => "290",
+                'name_sec' => "Название места"
+            ];
+        }
+
+        return view('pages.events.event')->withPlaces($places)->withSchemeData(json_encode($schemeData));
+    }
+}
